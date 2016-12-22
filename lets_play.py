641d3b9568cf6e865d91e53d69d5b13d0dc4f3fd
Matthew Rossi <matthewrossi@Matthews-MacBook-Pro-5.local>
2016-12-22 11:01:35 -0500

    more troubleshooting

--- lets_play.py
+++ lets_play.py
@@ -2,13 +2,15 @@ first = "Hello, "
 second = "what is your name?"
 
 original = raw_input(first + second)
-next_step = raw_input("Hello, " +   original + ". Would you like to play a game?") 
+name = original
+next_step = raw_input("Hello, " +   name + ". Would you like to play a game?") 
 
 if original.isalpha() and len(original) > 0:
+
     print next_step
 else:
     print "Come again?"
-    
+
 if  next_step == "y" or "Yes":
     print "Very good! Let's play!"
 else:
